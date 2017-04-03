3e86868b61a00ccc05ba7d66c711996a0730ea31
raghurammaturi <raghuram.maturi@gmail.com>
2017-04-03 10:55:31 -0400

    Add files via upload

---
+++ FileTestingApp/FileTestingApp/src/main/java/com/application/springbootstart/FileTestingAppApplication.java
@@ -0,0 +1,12 @@
+package com.application.springbootstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileTestingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileTestingAppApplication.class, args);
+	}
+}
